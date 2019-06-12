dbb61e553955ee9c2f3a2b31f434a6c5f3fbd78f
Jeremy School <jschool@catalyte.io>
2019-06-11 23:56:11 -0400

    current status

--- init.sql
+++ init.sql
@@ -1,4 +1,8 @@
-create table if not exists events (
+create schema if not exists EVENTS;
+create schema if not exists TEAMS;
+create schema if not exists MATCHES;
+
+create table if not exists EVENTS.events (
     id INT primary key not null,
     name VARCHAR(512),
     venue VARCHAR(128),
@@ -13,23 +17,18 @@ create table if not exists events (
     location_country VARCHAR(2)
 );
 
-create table if not exists teams (
+create table if not exists TEAMS.teams (
     id INT primary key not null,
     name VARCHAR(256),
     number VARCHAR(128)
 );
 
--- https://trello.com/c/VucNCBR5/16-add-match-data-table
-
-create table if not exists matches (
+create table if not exists MATCHES.matches (
     id VARCHAR(64) primary key not null, -- 4318-65532-5
-    matchId VARCHAR(64) not null, -- 65532-5
+    matchid VARCHAR(64) not null, -- 65532-5
     team INT not null, -- 4318
-    timeStamp BIGINT not null,
+    timestamp BIGINT not null,
     alliance VARCHAR(4) not null,
-    data VARCHAR(8192) not null
+    data VARCHAR(8192) not null,
+    number INT not null
 );
-
--- add number column to db
-
-alter table matches add column number INT not null;
